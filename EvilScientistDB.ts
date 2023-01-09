690724a6901d308388c5d56a7b54bd2a0babfa81
Jesper Flood <flod96@hotmail.com>
2023-01-09 13:30:28 +0100

    Nu har jag uppdaterat EvilScientistDB.ts och fixat en tsconfig fil

---
+++ EvilScientistDB.ts
@@ -0,0 +1,16 @@
+const evilScientists = {
+  scientist: [
+    {
+      name: "Jesper Flood",
+      age: 26,
+      amountOfHenchman: 22,
+      description: "Maskin som är maskin helt enkelt",
+    },
+    {
+      name: "Test",
+      age: 1,
+      amountOfHenchman: 1,
+      description: "Test person",
+    },
+  ],
+};
